2869d101b023970b8a350bbb73f6ff9549ecde28
That_Ski_Freak <64756991+That-Ski-Freak@users.noreply.github.com>
2023-08-20 03:28:33 -0500

    Script auto-commit

--- quartz/components/Graph.tsx
+++ quartz/components/Graph.tsx
@@ -25,7 +25,7 @@ const defaultOptions: GraphOptions = {
     drag: true,
     zoom: true,
     depth: 1,
-    scale: 1.1,
+    scale: 1.5,
     repelForce: 0.5,
     centerForce: 0.3,
     linkDistance: 30,
