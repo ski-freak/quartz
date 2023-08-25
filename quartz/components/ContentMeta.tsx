dce61f24c50c9b9debdb4962af2fbec73ae9c470
That_Ski_Freak <64756991+That-Ski-Freak@users.noreply.github.com>
2023-08-25 01:15:07 -0500

    Script auto-commit

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -8,8 +8,8 @@ export default (() => {
     if (text) {
       const segments: string[] = []
       const { text: timeTaken, words: _words } = readingTime(text)
-      if (fileData.dates?.modified) { //created
-        segments.push(formatDate(fileData.dates.modified)) //created
+      if (fileData.dates?.created) { //created
+        segments.push(formatDate(fileData.dates.created)) //created
       }
 
       segments.push(timeTaken)
