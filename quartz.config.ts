8a0872b0f9275d8e84805b44529920c9282840da
That_Ski_Freak <64756991+That-Ski-Freak@users.noreply.github.com>
2023-08-28 02:43:36 -0500

    Script auto-commit

--- quartz.config.ts
+++ quartz.config.ts
@@ -36,8 +36,8 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa", // 7b97aa
-          tertiary: "#84a59d", // 84a59d
+          secondary: "#91a3bb", // 7b97aa
+          tertiary: "#7b97aa", // 84a59d
           highlight: "rgba(143, 159, 169, 0.15)",
 //          currentgraph: "#f58382",
         },
